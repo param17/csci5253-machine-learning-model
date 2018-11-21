451e684a88eb8ef62937e1b62900d106c603e76a
Paramjot Singh <paramjot.singh@colorado.edu>
2018-11-21 08:09:10 -0700

    Correcting words in reviwS

--- python_ML_impl/aspect_extraction.py
+++ python_ML_impl/aspect_extraction.py
@@ -12,10 +12,11 @@ from textblob import TextBlob
 
 
 def clean_dataset(dataset):
-    cleaned_reviews = defaultdict(list)
+    cleaned_reviews = []
+    cleaned_reviews_dict = defaultdict(list)
     stop_words = set(stopwords.words('english'))
-    for i in range(len(dataset)):
-        sentence = re.sub(r"n\'t", " not", dataset['reviewText'][i])
+    for i, sentence in enumerate(dataset):
+        sentence = re.sub(r"n\'t", " not", sentence)
         sentence = re.sub(r"\'re", " are", sentence)
         sentence = re.sub(r"\'ll", " will", sentence)
         sentence = re.sub(r"\'s", " is", sentence)
@@ -31,7 +32,9 @@ def clean_dataset(dataset):
             review = sentence[k].split()
             review = [word.decode("utf-8") for word in review if not word in stop_words]
             sentence[k] = ' '.join(review)
-            cleaned_reviews[i].append(sentence[k])
+            sentence[k] = TextBlob(sentence[k]).correct()
+            cleaned_reviews_dict[i].append(sentence[k])
+        cleaned_reviews.append(cleaned_reviews_dict[i])
 
     return cleaned_reviews
 
@@ -39,6 +42,7 @@ def clean_dataset(dataset):
 with open(sys.argv[0]) as dataset_file:
     dataset = json.load(dataset_file)
 
-product_reviews = [dataset[index]['review'] for index in range(len(dataset))]
+product_reviews = clean_dataset([dataset[index]['review'] for index in range(len(dataset))])
 
 product_reviews_dataset = DataFrame({'reviewText': product_reviews})
+
